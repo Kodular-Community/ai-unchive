f1202bcdbb4f3df0a1ae4671372d993356b61496
Pavitra Golchha <hello@pavi2410.me>
2024-12-14 12:20:39 +0530

    fix blockly preview

--- src/components/BlocklyComponent.tsx
+++ src/components/BlocklyComponent.tsx
@@ -1,59 +1,60 @@
-import {useCallback, useRef} from 'react';
-import {getDescriptor} from "aia-kit/utils/utils.js";
+import { useCallback, useRef } from 'react';
+import { getDescriptor } from "aia-kit/utils/utils.js";
+import { useProject } from '../hooks/useProject.js';
 
 declare global {
-    const Blockly: any
-    const BlocklyWorkspace: any
+  const Blockly: any
+  const BlocklyWorkspace: any
 }
 
-function BlocklyComponent({blocksXml}: { blocksXml: string }) {
-    const wsRef = useRef<typeof BlocklyWorkspace | null>(null);
+function BlocklyComponent({ blocksXml }: { blocksXml: string }) {
+  const project = useProject();
+  const wsRef = useRef<typeof BlocklyWorkspace | null>(null);
 
-    const blocklyDivRef = useCallback((blocklyDiv: HTMLDivElement) => {
-        if (blocklyDiv) {
-            if (!wsRef.current) {
-                const ws = Blockly.inject(blocklyDiv, {
-                    readOnly: true,
-                    trashcan: false,
-                    toolbox: false,
-                    scrollbars: true,
-                    grid: {
-                        spacing: 20,
-                        length: 3,
-                        colour: '#ccc',
-                        snap: true,
-                    },
-                    // zoom: {
-                    //     controls: true,
-                    //     // wheel: true,
-                    //     startScale: 1.0,
-                    //     maxScale: 3,
-                    //     minScale: 0.3,
-                    //     scaleSpeed: 1.2,
-                    //     pinch: true,
-                    // },
-                });
+  const blocklyDivRef = useCallback((blocklyDiv: HTMLDivElement) => {
+    if (!wsRef.current) {
+      const ws = Blockly.inject(blocklyDiv, {
+        readOnly: true,
+        trashcan: false,
+        toolbox: false,
+        scrollbars: true,
+        grid: {
+          spacing: 20,
+          length: 3,
+          colour: '#ccc',
+          snap: true,
+        },
+        // zoom: {
+        //   controls: true,
+        //   // wheel: true,
+        //   startScale: 1.0,
+        //   maxScale: 3,
+        //   minScale: 0.3,
+        //   scaleSpeed: 1.2,
+        //   pinch: true,
+        // },
+      });
 
-                ws.getDescriptor = getDescriptor
-                const block = Blockly.Xml.domToWorkspace(Blockly.Xml.textToDom(blocksXml), ws)
+      ws.getDescriptor = (componentType: string) => getDescriptor(componentType, project)
 
-                wsRef.current = ws;
+      const block = Blockly.Xml.domToWorkspace(Blockly.Xml.textToDom(blocksXml), ws)
 
-                console.log('created new workspace...');
-            }
-        }
+      wsRef.current = ws;
 
-        // return () => {
-        //     wsRef.current?.dispose();
-        // }
-    }, [wsRef, blocksXml]);
+      console.log('created new workspace...');
+    }
 
-    return (
-        <div ref={blocklyDivRef} className="blocklyDiv" style={{
-            height: "calc(100dvh - var(--app-shell-header-height) - 80px)",
-            width: '100%'
-        }}/>
-    );
+    // return () => {
+    //     wsRef.current?.dispose();
+    // }
+  }, [wsRef, blocksXml]);
+
+  return (
+    <div ref={blocklyDivRef} className="blocklyDiv" style={{
+      height: "calc(100dvh - var(--app-shell-header-height) - 80px)",
+      width: '100%'
+    }} />
+  );
 }
 
 export default BlocklyComponent;
